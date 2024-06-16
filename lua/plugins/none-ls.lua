43415142ea63815afba5295f2443bf9f880d0531
Viliam Holly <viliamteoholly@gmail.com>
2024-06-16 19:36:43 +0200

    Make small addition

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,6 +5,8 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.formatting.zigfmt,
 			},
 		})
 		vim.keymap.set("n", "<leader>bf", vim.lsp.buf.format, { desc = "[F]ormat buffer" })
