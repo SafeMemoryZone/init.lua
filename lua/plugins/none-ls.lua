eae4f7edcc8d8943129952823558ce61b199a2e8
Viliam Holly <viliamteoholly@gmail.com>
2024-06-16 19:54:20 +0200

    Add another small change

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,7 +6,7 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.clang_format,
-        null_ls.builtins.formatting.zigfmt,
+        -- null_ls.builtins.formatting.zigfmt,
 			},
 		})
 		vim.keymap.set("n", "<leader>bf", vim.lsp.buf.format, { desc = "[F]ormat buffer" })
