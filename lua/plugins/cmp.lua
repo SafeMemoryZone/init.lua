fc105d1f98afdf9accb3214292d150f47a046a9b
Viliam Holly <viliamteoholly@gmail.com>
2024-06-17 18:07:47 +0200

    Remove main.c and add keybinding for luasnip

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,36 +1,51 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    {
-      "L3MON4D3/LuaSnip",
-      version = "v2.*",
-      build = "make install_jsregexp",
-    },
-  },
-  config = function()
-    local cmp = require("cmp")
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-      }),
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      sources = cmp.config.sources({
-        { name = "nvim_lsp", max_item_count = 5 },
-      }, {
-        { name = "buffer", max_item_count = 5 },
-      }),
-    })
-  end,
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		{
+			"L3MON4D3/LuaSnip",
+			version = "v2.*",
+			build = "make install_jsregexp",
+		},
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-]>"] = cmp.mapping(function(fallback)
+					if luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<C-[>"] = cmp.mapping(function(fallback)
+					if luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp", max_item_count = 5 },
+			}, {
+				{ name = "buffer", max_item_count = 5 },
+			}),
+		})
+	end,
 }
