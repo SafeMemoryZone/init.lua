4ea94782ea90014da009eda092f8550478a82850
Viliam Holly <viliamteoholly@gmail.com>
2024-08-09 14:49:33 +0200

    Add vim indenting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,163 +1,164 @@
 return {
-	{
-		"goolord/alpha-nvim",
-		config = function()
-			local alpha = require("alpha")
-			local dashboard = require("alpha.themes.startify")
-			dashboard.section.header.val = {
-				[[                                                    ]],
-				[[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
-				[[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
-				[[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
-				[[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
-				[[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
-				[[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
-				[[                                                    ]],
-			}
-			alpha.setup(dashboard.opts)
-		end,
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("tokyonight").setup({
+  {
+    "goolord/alpha-nvim",
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.startify")
+      dashboard.section.header.val = {
+        [[                                                    ]],
+        [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
+        [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
+        [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
+        [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+        [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+        [[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+        [[                                                    ]],
+      }
+      alpha.setup(dashboard.opts)
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
         transparent = true,
-				styles = {
-					comments = { bold = true },
-				},
-			})
-			vim.cmd("colorscheme tokyonight")
-		end,
-	},
-	{
-		"folke/todo-comments.nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { signs = false },
-		init = function()
-			vim.keymap.set("n", "<leader>tt", "<cmd>TodoTelescope<cr>")
-		end,
-	},
-	{ "ThePrimeagen/vim-be-good" },
-	{
-		"hedyhli/outline.nvim",
-		config = function()
-			vim.keymap.set("n", "<leader>ps", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
-			require("outline").setup({
-				keymaps = {
-					goto_and_close = "d",
-				},
-			})
-		end,
-	},
-	{
-		"ojroques/nvim-hardline",
-		config = function()
-			require("hardline").setup({
-				theme = "nordic",
-				sections = {
-					{ class = "mode", item = require("hardline.parts.mode").get_item },
-					{ class = "med", item = require("hardline.parts.filename").get_item },
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = true,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = { { "williamboman/mason.nvim", config = true } },
-		lazy = false,
-		opts = {
-			ensure_installed = {
-				"lua_ls",
-				"clangd",
-				"zls",
-				"pyright",
-				"html",
-				"cssls",
-				"denols",
-				"intelephense",
-				"cmake",
-			},
-		},
-	},
-	require("plugins/lspconfig"),
-	require("plugins/cmp"),
-	require("plugins/none-ls"),
-	require("plugins/treesitter"),
-	require("plugins/telescope"),
-	{
-		"Civitasv/cmake-tools.nvim",
-		opts = {
-			cmake_build_directory = "build",
-			cmake_soft_link_compile_commands = false,
-			cmake_compile_commands_from_lsp = true,
-		},
-		init = function()
-			vim.keymap.set("n", "<leader>mc", "<cmd>CMakeBuild<cr>")
-			vim.keymap.set("n", "<leader>mr", "<cmd>CMakeRun<cr>")
-			vim.keymap.set("n", "<leader>ms", "<cmd>CMakeTargetSettings<cr>")
-		end,
-	},
-	{
-		"stevearc/oil.nvim",
-		opts = {
-			{
-				skip_confirm_for_simple_edits = true,
-				prompt_save_on_select_new_entry = false,
-				{
-					keymaps = {
-						["<C-v>"] = "actions.select_vsplit",
-						["<C-s>"] = "actions.select_split",
-					},
-				},
-			},
-		},
-		init = function()
-			vim.keymap.set("n", "<leader>pv", function()
-				require("oil").open(require("oil").get_curr_dir)
-			end, { desc = "Open current dir" })
-		end,
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
-	{ "vim-utils/vim-man" },
-	{
-		"ThePrimeagen/harpoon",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		init = function()
-			vim.keymap.set("n", "<leader>mm", require("harpoon.mark").add_file, { desc = "[M]ark file" })
-			vim.keymap.set("n", "<C-m>", require("harpoon.ui").toggle_quick_menu, { desc = "Toggle quick [M]enu" })
-			vim.keymap.set("n", "<C-1>", function()
-				require("harpoon.ui").nav_file(1)
-			end, { desc = { "Navigate to file 1" } })
-			vim.keymap.set("n", "<C-2>", function()
-				require("harpoon.ui").nav_file(2)
-			end, { desc = { "Navigate to file 2" } })
-			vim.keymap.set("n", "<C-3>", function()
-				require("harpoon.ui").nav_file(3)
-			end, { desc = { "Navigate to file 3" } })
-			vim.keymap.set("n", "<C-4>", function()
-				require("harpoon.ui").nav_file(3)
-			end, { desc = { "Navigate to file 4" } })
-			vim.keymap.set("n", "<C-5>", function()
-				require("harpoon.ui").nav_file(3)
-			end, { desc = { "Navigate to file 5" } })
-		end,
-	},
-	{
-		"echasnovski/mini.nvim",
-		config = function()
-			require("mini.pairs").setup()
-			require("mini.comment").setup()
-			require("mini.surround").setup()
-		end,
-	},
+        styles = {
+          comments = { bold = true },
+        },
+      })
+      vim.cmd("colorscheme tokyonight")
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = { signs = false },
+    init = function()
+      vim.keymap.set("n", "<leader>tt", "<cmd>TodoTelescope<cr>")
+    end,
+  },
+  { "ThePrimeagen/vim-be-good" },
+  {
+    "hedyhli/outline.nvim",
+    config = function()
+      vim.keymap.set("n", "<leader>ps", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+      require("outline").setup({
+        keymaps = {
+          goto_and_close = "d",
+        },
+      })
+    end,
+  },
+  {
+    "ojroques/nvim-hardline",
+    config = function()
+      require("hardline").setup({
+        theme = "nordic",
+        sections = {
+          { class = "mode", item = require("hardline.parts.mode").get_item },
+          { class = "med",  item = require("hardline.parts.filename").get_item },
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { { "williamboman/mason.nvim", config = true } },
+    lazy = false,
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "clangd",
+        "zls",
+        "pyright",
+        "html",
+        "cssls",
+        "quick_lint_js",
+        "phpactor",
+        "cmake",
+      },
+    },
+  },
+  require("plugins/lspconfig"),
+  require("plugins/cmp"),
+  require("plugins/none-ls"),
+  require("plugins/treesitter"),
+  require("plugins/telescope"),
+  {
+    "Civitasv/cmake-tools.nvim",
+    opts = {
+      cmake_build_directory = "build",
+      cmake_soft_link_compile_commands = false,
+      cmake_compile_commands_from_lsp = true,
+    },
+    init = function()
+      vim.keymap.set("n", "<leader>mc", "<cmd>CMakeBuild<cr>")
+      vim.keymap.set("n", "<leader>mr", "<cmd>CMakeRun<cr>")
+      vim.keymap.set("n", "<leader>ms", "<cmd>CMakeTargetSettings<cr>")
+    end,
+  },
+  {
+    "stevearc/oil.nvim",
+    opts = {
+      {
+        skip_confirm_for_simple_edits = true,
+        prompt_save_on_select_new_entry = false,
+        {
+          keymaps = {
+            ["<C-v>"] = "actions.select_vsplit",
+            ["<C-s>"] = "actions.select_split",
+          },
+        },
+      },
+    },
+    init = function()
+      vim.keymap.set("n", "<leader>pv", function()
+        require("oil").open(require("oil").get_curr_dir)
+      end, { desc = "Open current dir" })
+    end,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  { "vim-utils/vim-man" },
+  {
+    "ThePrimeagen/harpoon",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    init = function()
+      vim.keymap.set("n", "<leader>mm", require("harpoon.mark").add_file, { desc = "[M]ark file" })
+      vim.keymap.set("n", "<C-m>", require("harpoon.ui").toggle_quick_menu, { desc = "Toggle quick [M]enu" })
+      vim.keymap.set("n", "<C-1>", function()
+        require("harpoon.ui").nav_file(1)
+      end, { desc = { "Navigate to file 1" } })
+      vim.keymap.set("n", "<C-2>", function()
+        require("harpoon.ui").nav_file(2)
+      end, { desc = { "Navigate to file 2" } })
+      vim.keymap.set("n", "<C-3>", function()
+        require("harpoon.ui").nav_file(3)
+      end, { desc = { "Navigate to file 3" } })
+      vim.keymap.set("n", "<C-4>", function()
+        require("harpoon.ui").nav_file(3)
+      end, { desc = { "Navigate to file 4" } })
+      vim.keymap.set("n", "<C-5>", function()
+        require("harpoon.ui").nav_file(3)
+      end, { desc = { "Navigate to file 5" } })
+    end,
+  },
+  {
+    "echasnovski/mini.nvim",
+    config = function()
+      require("mini.pairs").setup()
+      require("mini.comment").setup()
+      require("mini.surround").setup()
+    end,
+  },
+  { "tpope/vim-sleuth" },
 }
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -12,8 +12,8 @@ return {
 		lspconfig.pyright.setup({ capabilities = capabilities })
 		lspconfig.html.setup({ capabilities = capabilities })
 		lspconfig.cssls.setup({ capabilities = capabilities })
-		lspconfig.denols.setup({ capabilities = capabilities })
-		lspconfig.intelephense.setup({ capabilities = capabilities })
+		lspconfig.quick_lint_js.setup({ capabilities = capabilities })
+		lspconfig.phpactor.setup({ capabilities = capabilities })
 		lspconfig.cmake.setup({ capabilities = capabilities })
 
 		vim.api.nvim_create_autocmd("LspAttach", {
