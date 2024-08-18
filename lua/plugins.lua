9ca368abc9ae7d52c4aa202c1414cb2234282dd5
Viliam Holly <viliamteoholly@gmail.com>
2024-08-18 13:44:59 +0200

    Remoec cppcheck

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -141,9 +141,6 @@ return {
 				sources = {
 					null_ls.builtins.formatting.stylua,
 					null_ls.builtins.formatting.clang_format,
-					null_ls.builtins.diagnostics.cppcheck.with({
-						extra_args = { "--check-level=exhaustive" },
-					}),
 				},
 			})
 			vim.keymap.set("n", "<leader>bf", vim.lsp.buf.format, { desc = "[F]ormat buffer" })
